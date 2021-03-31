4eb4a0007e3203259cde4014e3b17a541c8b71e1
Juan <juanba98@uma.es>
2021-03-31 04:01:27 -0700

    base proyecto

---
+++ src/main/resources/es/uma/informatica/jpa/tarea1/Asignaturas_Matricula.java
@@ -0,0 +1,21 @@
+package es.uma.informatica.jpa.tarea1;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Asignaturas_Matricula
+ *
+ */
+@Entity
+
+public class Asignaturas_Matricula implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public Asignaturas_Matricula() {
+		super();
+	}
+   
+}
